32297e408be70e42bfb198ff62effef6070a67e1
Avinash098o <90516887+Avinash098o@users.noreply.github.com>
2021-09-18 12:23:28 +0530

    project1

---
+++ and - Copy.java
@@ -0,0 +1,63 @@
+package com.avi;
+
+import java.util.Scanner;
+
+public class and {
+    public static void main(String[] args) {
+            /*int temp = 18;
+            boolean warm = temp>20 && temp < 30;
+        System.out.println(warm);*/
+
+        /*boolean x = true;
+        boolean y = false;
+        boolean z =false;
+        boolean alphabet = (x||y) &&! z;
+        System.out.println(alphabet);*/
+        /*int a = 10101;
+        short b =(short) a;
+        System.out.println(~a);
+*/
+        Scanner av = new Scanner(System.in);
+        System.out.println("What is your name ");
+        String ads = av.nextLine();
+        System.out.println("My name is" + ads);
+
+        if (ads.length() <= 8) {
+            System.out.println("yes");
+        }
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
